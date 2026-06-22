5f6f8524ac4e12b5dea0f2e209f8cc8dec6e7f09
Narith-hen <narith.hen2025@gmail.com>
2026-06-22 09:05:06 +0700

    feat: categories and products

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,21 +15,33 @@ Route::get('/user', function (Request $request) {
 |-------------------------------------------------------------------------- 
 | Public Routes 
 |-------------------------------------------------------------------------- 
-*/ 
- 
-Route::post('/register', [AuthController::class, 'register']); 
-Route::post('/login', [AuthController::class, 'login']); 
+*/
 
- 
-Route::middleware('auth:sanctum')->group(function () { 
- 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+
     // Authentication 
-    Route::get('/profile', [AuthController::class, 'profile']); 
+    Route::get('/profile', [AuthController::class, 'profile']);
     Route::post('/profile', [AuthController::class, 'updateProfile']);
-    Route::post('/logout', [AuthController::class, 'logout']); 
- 
-// Categories 
-// Route::apiResource('categories', CategoryController::class); 
-}); 
-// Products 
-// Route::apiResource('products', ProductController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    //  protect route (private)
+    Route::post('/categories', [CategoryController::class, 'store'])
+        ->name('api.categories.store');
+    Route::put('/categories/{category}', [CategoryController::class, 'update'])
+        ->name('api.categories.update');
+    Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])
+        ->name('api.categories.destroy');
+
+    // public route
+    Route::get('/categories', [CategoryController::class, 'index'])
+        ->name('api.categories.index');
+    Route::get('/categories/{category}', [CategoryController::class, 'show'])
+        ->name('api.categories.show');
+
+    // Products 
+    Route::apiResource('products', ProductController::class);
+});
--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// category route:
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
+Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+Route::get('/categories/{id}', [CategoryController::class, 'edit'])->name('categories.edit');
+Route::put('/categories/{id}', [CategoryController::class, 'update'])->name('categories.update');
+Route::delete('/categories/{id}', [CategoryController::class, 'destroy'])->name('categories.destroy');
